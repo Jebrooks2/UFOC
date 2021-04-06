44be122e37ed6d8bc4028e40682a6f5569636844
ERJennings <erjennings13@gmail.com>
2021-04-06 17:03:01 -0400

    Added login page

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<html>
+<h1>
+    Welcome to the UFOC System
+</h1>
 <?php
+echo "<html><body><form index=\"index.php\" method = post>
+    
+    <input class = \"text\" type = \"number\" name = \"username\">
+    <br>
+    <input class = \"text\" type = \"number\" name = \"password\"><br>
+    
+    <button class = \"button\" type=\"submit\" name=\"login\">Log In</button>
+</form>"
+?>
+</html>
