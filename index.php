5d6f5443cba6a0d0e9fb9cde960b0f73fc2c7df1
ERJennings <erjennings13@gmail.com>
2021-04-06 17:22:24 -0400

    Added style and finished login page

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <html>
-<h1>
-    Welcome to the UFOC System
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UFOC</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<h1 class = h1>
+    Welcome to the UFOC System!
 </h1>
 <?php
-echo "<html><body><form index=\"index.php\" method = post>
+echo "<html><body class = \"body\"><form index=\"index.php\" method = post>
     
-    <input class = \"text\" type = \"number\" name = \"username\">
+    <h2 class = h2>Username</h2>
+    <input class = \"input\" type = \"text\" name = \"username\">
     <br>
-    <input class = \"text\" type = \"number\" name = \"password\"><br>
+    <h2 class = h2>Password</h2>
+    <input class = \"input\" type = \"text\" name = \"password\"><br>
     
     <button class = \"button\" type=\"submit\" name=\"login\">Log In</button>
 </form>"
 ?>
+</body>
 </html>
